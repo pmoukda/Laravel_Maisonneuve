ba5509fdf638327416a03b0d2d17a90557a6451e
pmoukda <moukda_p@hotmail.com>
2025-09-26 12:15:13 -0400

    Added VilleFactory and EtudiantFactory files

---
+++ database/factories/EtudiantFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Ville;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Etudiant>
+ */
+class EtudiantFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+        'nom' => $this->faker->name(),
+        'adresse' => $this->faker->address(),
+        'telephone' => $this->faker->phoneNumber(),
+        'email' =>$this->faker->unique()->safeEmail(),
+        'date_de_naissance' => $this->faker->date(),
+        'ville_id'=> Ville::factory()
+        ];
+    }
+}
