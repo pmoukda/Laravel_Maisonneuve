76011292be6fc6d266e03b45401d5f9c133de561
pmoukda <moukda_p@hotmail.com>
2025-11-01 00:13:23 -0400

    Add create and store method

---
+++ app/Http/Controllers/ForumController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Forum;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Symfony\Contracts\Service\Attribute\Required;
+use Illuminate\Support\Facades\Auth;
+
+class ForumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('forum.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request-> validate([
+            'title_en' => 'max:100',
+            'title_fr' => 'required|max:100',
+            'content_en' => 'max:1100',
+            'content_fr' => 'required|max:1100',
+            'published_at' => 'date|nullable',
+            'language' => 'required|in:fr,en',
+            
+        ],
+        [],
+        [
+            'title_en' => trans('lang.title_en'),
+            'title_fr' => trans('lang.title_fr'),
+            'content_en' => trans('lang.content_en'),
+            'content_fr' => trans('lang.content_fr'),
+            'published_at' => trans('lang.publish_at'),
+            'language' => trans('lang.language'),
+            
+        ]);
+        
+        // Filtrer les langues remplies
+        $titles = array_filter([
+            'fr' => $request->input('title_fr'),
+            'en' => $request->input('title_en'),
+        ]);
+
+        $contents = array_filter([
+            'fr' => $request->input('content_fr'),
+            'en' => $request->input('content_en'),
+        ]);
+
+        $forum = Forum::create([
+            'language' => $request->input('language'),
+            'title' => $titles,
+            'content' => $contents,
+            'published_at' => $request->input('published_at'),
+            'user_id' => Auth::id(),
+        ]);
+
+        return redirect()->route('forum.show', $forum->id)->with('success', trans('lang.success_create_forum_msg'));
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Forum $forum)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Forum $forum)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Forum $forum)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Forum $forum)
+    {
+        //
+    }
+}
