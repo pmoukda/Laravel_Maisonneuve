0c3ed11f8ab0855a13f9da81f05619b4d5f22571
pmoukda <moukda_p@hotmail.com>
2025-11-05 15:28:32 -0500

    Create table folders

---
+++ database/migrations/2025_11_03_040407_create_folders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('folders', function (Blueprint $table) {
+            $table->id();
+            $table->json('title');
+            $table->timestamp('published_at');
+            $table->string('language');  
+            $table->string('path')->nullable();  
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('folders');
+    }
+};
