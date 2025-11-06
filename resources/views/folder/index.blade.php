de51395b0c055f868da3875959d234bd0ffb5b4f
pmoukda <moukda_p@hotmail.com>
2025-11-05 23:44:21 -0500

    Update index file

--- resources/views/folder/index.blade.php
+++ resources/views/folder/index.blade.php
@@ -9,16 +9,24 @@
             <tr>
                 <th class="col-md-5 px-4">@lang('lang.text_folderTable_title')</th>
                 <th class="col-md-5">@lang('lang.text_folderTable_user_name')</th>
-                <th class="col-md-5">@lang('lang.text_folderTable_link')</th>',
+                <th class="col-md-5">@lang('lang.text_folderTable_link')</th>
+                <th class="col-md-5">@lang('lang.text_Edit_btn')</th>
+                <th class="col-md-5">@lang('lang.text_Delete_btn')</th>
             </tr>
         </thead>
         <tbody class="table-group-divider">
             @forelse ($folders as $folder )
-            <tr>
-                <td class="px-4">{{ $folder->title }}</td>
-                <td>{{ $folder->user->name }}</td>
-                <td><a href="{{ route('folder.show', $folder->id) }}">@lang('lang.text_folderTable_body')</a></td>
-            </tr>
+            @auth
+                <tr>
+                    <td class="px-4">{{ $folder->title[$folder->language] }}</td>
+                    <td>{{ $folder->user->name }}</td>
+                    <td><a href="{{ asset('storage/' . $folder->path) }}" target="_blank">@lang('lang.text_folderTable_body')</a></td>
+                @if (auth()->id() === $forum->user_id )
+                    <td><a href="{{ route('folder.edit',$folder->id) }}" class="btn btn-warning fs-6"><i class="bi bi-pencil-square"></i></a></td>
+                    <td><button type="submit" class="btn btn-danger fs-6"><i class="bi bi-trash3"></i></button></td>
+                @endif
+                </tr>
+            @endauth
             @empty
             <div class="alert alert-warning mb-4 text-center fs-4">
                @lang('lang.text_folder_alert_msg')
